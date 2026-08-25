d067d5cd03a700bda203bf47f74c7d91ed3c39cb
HammerPot <potter@hammerpot.dev>
2026-08-24 21:59:11 -0400

    update flake lock

--- modules/home/niri/default.nix
+++ modules/home/niri/default.nix
@@ -21,7 +21,7 @@
       restartIfChanged = true;   # Auto-restart dms.service when dank-material-shell changes
     };
 
-    enableSystemMonitoring = true;     # System monitoring widgets (dgop)
+    enableSystemMonitoring = true;     # System monitoring widgets (dggiop)
     enableVPN = true;                  # VPN management widget
     enableDynamicTheming = true;       # Wallpaper-based theming (matugen)
     enableAudioWavelength = true;      # Audio visualizer (cava)
