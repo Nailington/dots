a8faf5afe0174c428febdf0a88d75d4030e0bca9
HammerPot <potter@hammerpot.dev>
2026-08-24 22:10:20 -0400

    Twitch miner moment

--- pkgs/twitch-drops-miner/default.nix
+++ pkgs/twitch-drops-miner/default.nix
@@ -18,7 +18,7 @@ let
   # AppImage is shipped inside a zip on the dev-build release
   zip = fetchurl {
     url = "https://github.com/DevilXD/TwitchDropsMiner/releases/download/dev-build/Twitch.Drops.Miner.Linux.AppImage-x86_64.zip";
-    hash = "sha256-BAtnjUSSpSYtHo1AjOLCRYbOfwI71rDxvh3tOsGpRco=";
+    hash = "sha256-HLMHuP6OnFNusy2Tquuen7fmClJcNZJeIOj6PxuToBo=";
   };
 
   # Extract AppImage from zip (zip contains single .AppImage file)
