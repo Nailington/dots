8d4ba530d2e21f003e70b2773e677de18573b0d8
HammerPot <potter@hammerpot.dev>
2026-09-16 22:58:30 -0400

    lazy? yes I am lazy.

--- hosts/ewbtciast/default.nix
+++ hosts/ewbtciast/default.nix
@@ -7,7 +7,11 @@
 
   # 2017 Intel iMac (OCLP). Keep eval/build load down.
   # nix.settings is ignored while Determinate owns nix.conf; this goes to nix.custom.conf.
-  determinateNix.customSettings.max-jobs = 2;
+  determinateNix.customSettings = {
+    max-jobs = 2;
+    lazy-trees = true;
+    eval-cores = 1;
+  };
 
   system.primaryUser = "potter";
   system.stateVersion = 6;
